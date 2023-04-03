9d8d88aae4b5ce2eba6011edaa7b6f6ab2178e7c
vwaiolet <passwordk@kakao.com>
2023-04-03 17:30:07 +0900

    homework

---
+++ DataStructure/stack.py
@@ -0,0 +1,9 @@
+st1 = ListStack()
+print(st1.top())	# No effect
+st1.push(100)
+st1.push(200)
+print("Top is", st1.top())
+st1.pop()
+st1.push('Monday')
+st1.printStack()
+print('isEmpty?', st1.isEmpty())
