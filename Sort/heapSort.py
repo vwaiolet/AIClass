9cbe7e742434a3879fbe72d7e21af0915775672f
vwaiolet <passwordk@kakao.com>
2023-04-07 20:46:33 +0900

    23.04.05

--- Sort/heapSort.py
+++ Sort/heapSort.py
@@ -1,14 +1,31 @@
-def heapSort(arr):
-
-
+def heapSort(A):
+    buildHeap(A)
+    for last in range(len(A) - 1, 0 ,-1):
+        A[last], A[0] = A[0], A[last]
+        percolateDown(A, 0, last-1)
+        
+def buildHeap(A):
+    for i in range((len(A)-2) // 2, -1, -1):
+        percolateDown(A, i, len(A)-1)
+        
+        
+def percolateDown(A, k:int, end:int):
+    child = 2 * k + 1  # left child
+    right = 2 * k + 2  # right child
+    if child <= end:
+            if right <= end and A[child] < A[right]:
+                    child = right
+                
+            if A[k] < A[child]:
+                A[k], A[child] = A[child], A[k]
+                percolateDown(A,child,end)     
 
 def main():
-    print('Heapsort!')
-    a = [3, 8, 2, 4, 8, 1, 2, 0, 5, 9]
-    print('a[]:       ', a)
-    heapSort(a)
-    print('Sorted a[]:', a)
+    print("Heapsort ! ")
+    A = [3,8, 2, 4, 8, 1, 2, 0, 5, 9]
+    print("A[]:             ",A)
+    heapSort(A)
+    print("Sorted A[]:", A)
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main()                                                                               				
